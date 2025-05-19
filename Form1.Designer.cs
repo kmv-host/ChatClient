1f90f59a1d7e7b9d7dc196eee4ad676c9ac30645
Afonichkin Andrey <gorlum1241@mail.ru>
2025-05-19 16:31:45 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace ChatClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtChat = new System.Windows.Forms.TextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(12, 43);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(253, 20);
+            this.txtIP.TabIndex = 0;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(284, 43);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(61, 20);
+            this.txtPort.TabIndex = 1;
+            // 
+            // txtChat
+            // 
+            this.txtChat.Location = new System.Drawing.Point(12, 89);
+            this.txtChat.Multiline = true;
+            this.txtChat.Name = "txtChat";
+            this.txtChat.Size = new System.Drawing.Size(333, 289);
+            this.txtChat.TabIndex = 2;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(79, 408);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(266, 20);
+            this.txtMessage.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Введите IP сервера";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(281, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "порт";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(76, 392);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "введите  сообщение";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(15, 452);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 7;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(270, 452);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 8;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(12, 408);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(56, 20);
+            this.txtUsername.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 392);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "имя";
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(146, 452);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(75, 23);
+            this.btnDisconnect.TabIndex = 11;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(363, 493);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtChat);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtIP);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.TextBox txtChat;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnDisconnect;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;         // Добавили пространство имен
+using System.Net.Sockets; //Добавили пространство имен
+using System.Threading;   //Добавили пространство имен
+
+namespace ChatClient
+{
+    public partial class Form1 : Form
+    {
+        private TcpClient client;
+        private NetworkStream stream;
+        private bool isConnected = false;
+        private readonly object lockObject = new object();
+
+        public Form1()
+        {
+            InitializeComponent();
+            btnDisconnect.Enabled = false;
+            btnSend.Enabled = false;
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtUsername.Text))
+            {
+                MessageBox.Show("Введите имя пользователя", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (!IPAddress.TryParse(txtIP.Text, out _) && !txtIP.Text.Equals("localhost", StringComparison.OrdinalIgnoreCase))
+            {
+                MessageBox.Show("Введите корректный IP-адрес или 'localhost'", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (!int.TryParse(txtPort.Text, out int port) || port < 1 || port > 65535)
+            {
+                MessageBox.Show("Введите корректный порт (1-65535)", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                client = new TcpClient();
+                client.Connect(txtIP.Text, port);
+                stream = client.GetStream();
+                isConnected = true;
+
+                // Отправляем имя серверу
+                byte[] nameData = Encoding.UTF8.GetBytes($"NAME:{txtUsername.Text}");
+                stream.Write(nameData, 0, nameData.Length);
+
+                Thread receiveThread = new Thread(ReceiveMessages);
+                receiveThread.IsBackground = true;
+                receiveThread.Start();
+
+                UpdateUIOnConnect();
+                UpdateChat($"Подключен к серверу {txtIP.Text}:{txtPort.Text} как {txtUsername.Text}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка подключения: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Disconnect();
+            }
+        }
+
+        private void UpdateUIOnConnect()
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action(UpdateUIOnConnect));
+                return;
+            }
+
+            btnConnect.Enabled = false;
+            btnDisconnect.Enabled = true;
+            btnSend.Enabled = true;
+            txtUsername.Enabled = false;
+            txtIP.Enabled = false;
+            txtPort.Enabled = false;
+        }
+
+        private void UpdateUIOnDisconnect()
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action(UpdateUIOnDisconnect));
+                return;
+            }
+
+            btnConnect.Enabled = true;
+            btnDisconnect.Enabled = false;
+            btnSend.Enabled = false;
+            txtUsername.Enabled = true;
+            txtIP.Enabled = true;
+            txtPort.Enabled = true;
+        }
+
+        private void ReceiveMessages()
+        {
+            byte[] buffer = new byte[4096]; // Увеличили размер буфера
+            try
+            {
+                while (isConnected)
+                {
+                    int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                    if (bytesRead == 0) break;
+
+                    string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    UpdateChat(message);
+                }
+            }
+            catch
+            {
+                UpdateChat("Соединение с сервером потеряно");
+            }
+            finally
+            {
+                Disconnect();
+            }
+        }
+
+        private void UpdateChat(string message)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action<string>(UpdateChat), message);
+                return;
+            }
+            txtChat.AppendText($"{message}\r\n");
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (!isConnected) return;
+
+            string message = txtMessage.Text.Trim();
+            if (string.IsNullOrEmpty(message)) return;
+
+            try
+            {
+                byte[] data = Encoding.UTF8.GetBytes(message);
+                lock (lockObject)
+                {
+                    stream.Write(data, 0, data.Length);
+                }
+                txtChat.AppendText($"{txtUsername.Text}: {message}\r\n");
+                txtMessage.Clear();
+            }
+            catch (Exception ex)
+            {
+                UpdateChat($"Ошибка отправки: {ex.Message}");
+                Disconnect();
+            }
+        }
+
+
+        private void Disconnect()
+        {
+            if (!isConnected) return;
+
+            isConnected = false;
+            try
+            {
+                stream?.Close();
+                client?.Close();
+            }
+            catch { }
+
+            UpdateUIOnDisconnect();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Disconnect();
+        }
+
+        private void txtMessage_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && !string.IsNullOrWhiteSpace(txtMessage.Text))
+            {
+                btnSend_Click(sender, e);
+                e.SuppressKeyPress = true; // Предотвращаем звуковой сигнал
+            }
+        }
+
+        private void btnDisconnect_Click_1(object sender, EventArgs e)
+        {
+            Disconnect();
+            UpdateChat("Отключен от сервера");
+        }
+    }
+}
